07e145ba42dd0ee9440f4908cb2622ebbabc61cc
Nicholas Choong <nicholascwc1997@gmail.com>
2021-06-24 23:50:04 +0800

    Added edit feature, remove and edit icons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,33 @@
 import React from "react";
+import TodoForm from "./TodoForm";
+import { MdClose, MdModeEdit } from "react-icons/md";
 
-function Todo() {
-    return (<div></div>);
+function Todo({ todos, completeTodo, removeTodo }) {
+    const [edit, setEdit] = React.useState({
+        id: null,
+        value: "",
+    });
+
+    return todos.map((todo, index) => (
+        <div
+            className={todo.isComplete ? "todo-row complete" : "todo-row"}
+            key={index}
+        >
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className="icons">
+                <MdClose
+                    onClick={() => removeTodo(todo.id)}
+                    className="delete-icon"
+                />
+                <MdModeEdit
+                    onClick={() => setEdit(todo.id)}
+                    className="edit-icon"
+                />
+            </div>
+        </div>
+    ));
 }
 
 export default Todo;
